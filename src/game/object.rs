fa2b1c30a85388b6f730b9b3aacae5cfc2fc1553
sinka <xgao@zoyoe.com>
2024-03-14 14:39:22 +1100

    add loot

--- src/game/object.rs
+++ src/game/object.rs
@@ -1,5 +1,7 @@
 use serde::Serialize;
 
+use crate::tile::coordinate::{Coordinate, RectCoordinate, RectDirection};
+
 #[derive (Clone, Serialize)]
 pub struct Monster {
     pub hp: u64,
@@ -18,16 +20,74 @@ impl Monster {
 }
 
 #[derive (Clone, Serialize)]
-pub struct Tower{
+pub struct Tower<Direction: Clone + Serialize>{
     range: u64,
     power: u64,
+    pub cooldown: u64,
+    pub count: u64,
+    direction: Direction,
 }
 
-impl Tower {
-    pub fn new(range: u64, power: u64) -> Self {
+impl Tower<RectDirection> {
+    pub fn new(range: u64, power: u64, cooldown: u64, count: u64,  direction: RectDirection) -> Self {
         Tower {
             range,
-            power
+            power,
+            cooldown,
+            count,
+            direction
+        }
+    }
+    pub fn range(&self, src: &RectCoordinate, target: &RectCoordinate) -> usize {
+        let src = src.repr();
+        let target = target.repr();
+        match self.direction {
+            RectDirection::Left => {
+                if src.1 == target.1 {
+                    if target.0 < src.0 {
+                        (src.0 - target.0) as usize
+                    } else {
+                        usize::max_value()
+                    }
+                } else {
+                    usize::max_value()
+                }
+            },
+            RectDirection::Right => {
+                if src.1 == target.1 {
+                    if target.0 > src.0 {
+                        (target.0 - src.0) as usize
+                    } else {
+                        usize::max_value()
+                    }
+                } else {
+                    usize::max_value()
+                }
+            },
+            RectDirection::Top => {
+                if src.0 == target.0 {
+                    if target.1 < src.1 {
+                        (src.1 - target.1) as usize
+                    } else {
+                        usize::max_value()
+                    }
+                } else {
+                    usize::max_value()
+                }
+            },
+            RectDirection::Bottom => {
+                if src.0 == target.0 {
+                    if target.1 > src.1 {
+                        (target.1 - src.1) as usize
+                    } else {
+                        usize::max_value()
+                    }
+                } else {
+                    usize::max_value()
+                }
+
+            },
+
         }
     }
 }
@@ -60,13 +120,27 @@ impl Collector {
     }
 }
 
+#[derive (Clone, Serialize)]
+pub struct Dropped {
+    delta: u64,
+}
+
+impl Dropped {
+    pub fn new(delta: u64) -> Self {
+        Dropped {
+           delta
+        }
+    }
+}
+
 
 
 #[derive (Clone, Serialize)]
-pub enum Object {
+pub enum Object<Direction: Clone + Serialize> {
     Monster(Monster),
-    Tower(Tower),
+    Tower(Tower<Direction>),
     Spawner(Spawner),
+    Dropped(Dropped),
     Collector(Collector),
 }
 
--- src/game/state.rs
+++ src/game/state.rs
@@ -8,13 +8,14 @@ use crate::tile::coordinate::Coordinate;
 use crate::tile::map::PositionedObject;
 use super::object::Collector;
 use super::object::Monster;
+use super::object::Dropped;
 use super::object::Object;
 use super::object::Spawner;
 use super::object::Tower;
 
 // The global state
 pub struct State {
-    pub map: Map<RectCoordinate, Object>,
+    pub map: Map<RectCoordinate, Object<RectDirection>>,
 
 }
 
@@ -33,7 +34,9 @@ fn cor_to_index(x:usize, y:usize) -> usize {
 
 pub fn init_state() {
     let monster = Monster::new(10, 5, 1);
-    let tower = Tower::new(5, 1);
+    let tower = Tower::new(5, 1, 3, 3, RectDirection::Top);
+    let tower_left = Tower::new(5, 1, 4, 4, RectDirection::Left);
+    let tower_right = Tower::new(5, 1, 4, 4, RectDirection::Right);
     let spawner = Spawner::new(4, 4);
     let collector = Collector::new(5);
     let global = unsafe {&mut GLOBAL};
@@ -50,13 +53,41 @@ pub fn init_state() {
     global.map.set_feature(cor_to_index(2,2), Some(RectDirection::Bottom));
     global.map.set_feature(cor_to_index(2,3), Some(RectDirection::Right));
     global.map.set_feature(cor_to_index(3,3), Some(RectDirection::Right));
-    global.map.set_feature(cor_to_index(4,3), Some(RectDirection::Bottom));
+    global.map.set_feature(cor_to_index(4,3), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(5,3), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(6,3), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(7,3), Some(RectDirection::Bottom));
+    global.map.set_feature(cor_to_index(7,4), Some(RectDirection::Bottom));
+    global.map.set_feature(cor_to_index(7,5), Some(RectDirection::Left));
+    global.map.set_feature(cor_to_index(6,5), Some(RectDirection::Left));
+    global.map.set_feature(cor_to_index(5,5), Some(RectDirection::Left));
+    global.map.set_feature(cor_to_index(4,5), Some(RectDirection::Left));
+    global.map.set_feature(cor_to_index(3,5), Some(RectDirection::Bottom));
+    global.map.set_feature(cor_to_index(3,6), Some(RectDirection::Bottom));
+    global.map.set_feature(cor_to_index(3,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(4,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(5,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(6,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(7,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(8,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(9,7), Some(RectDirection::Right));
+    global.map.set_feature(cor_to_index(10,7), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,6), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,5), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,4), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,3), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,2), Some(RectDirection::Top));
+    global.map.set_feature(cor_to_index(10,1), Some(RectDirection::Top));
 
     global.map.spawn_at(Object::Monster(monster), RectCoordinate::new(0,0));
     global.map.spawn_at(Object::Spawner(spawner), RectCoordinate::new(0,0));
     global.map.spawn_at(Object::Tower(tower.clone()), RectCoordinate::new(3,4));
+    global.map.spawn_at(Object::Tower(tower_left), RectCoordinate::new(8,5));
+    global.map.spawn_at(Object::Tower(tower_right.clone()), RectCoordinate::new(9,6));
+    global.map.spawn_at(Object::Tower(tower_right.clone()), RectCoordinate::new(9,3));
+    global.map.spawn_at(Object::Tower(tower_right), RectCoordinate::new(9,2));
     global.map.spawn_at(Object::Tower(tower), RectCoordinate::new(1,4));
-    global.map.spawn_at(Object::Collector(collector), RectCoordinate::new(4,4));
+    global.map.spawn_at(Object::Collector(collector), RectCoordinate::new(10,0));
 }
 
 pub fn handle_move(obj_index: usize, pos: usize) {
@@ -82,18 +113,52 @@ pub fn handle_run() {
     }
     let mut termination = vec![];
     let mut spawn = vec![];
+    let mut tower_range:Vec<(Tower<RectDirection>, RectCoordinate, usize, usize, usize)> = vec![];
+
     for (index, obj) in objs.iter_mut().enumerate() {
-        if let Object::Monster(m) = &mut obj.object {
-            let x = obj.position.repr().0;
-            if x == 1 || x ==3 {
+        if let Object::Tower(tower) = &mut obj.object {
+            if tower.count == 0 {
+                tower_range.push((tower.clone(), obj.position.clone(), usize::max_value(), index, usize::max_value()));
+            } else {
+                tower.count -= 1;
+            }
+        }
+    }
+
+    for (index, obj) in objs.iter_mut().enumerate() {
+        if let Object::Monster(_) = &mut obj.object {
+            for t in tower_range.iter_mut() {
+                let range = t.0.range(&t.1, &obj.position);
+                if range < t.2 {
+                    t.2 = range;
+                    t.4 = index;
+                }
+            }
+        }
+    }
+
+    for t in tower_range.iter_mut() {
+        if t.4 != usize::max_value() {
+            if let Object::Monster(m) = &mut objs[t.4].object {
                 if m.hp < 2 {
                     m.hp = 0;
-                    termination.push(index);
                 } else {
                     m.hp -= 2;
                 }
+                if m.hp == 0 {
+                    termination.push(t.4);
+                    spawn.push(PositionedObject::new(Object::Dropped(Dropped::new(10)), objs[t.4].position.clone()));
+                }
             }
+            if let Object::Tower(tower) = &mut objs[t.3].object {
+                tower.count = tower.cooldown;
+            }
+        }
+    }
 
+
+    for (index, obj) in objs.iter_mut().enumerate() {
+        if let Object::Monster(_) = &mut obj.object {
             if collector.contains(&obj.position) {
                 termination.push(index);
             } else {
@@ -105,6 +170,19 @@ pub fn handle_run() {
                 }
             }
         }
+        else if let Object::Dropped(_) = &mut obj.object {
+            if collector.contains(&obj.position) {
+                termination.push(index);
+            } else {
+                let index = map.index_of_tile_coordinate(&obj.position);
+                let feature = map.get_feature(index);
+                if let Some(f) = feature {
+                    unsafe {wasm_dbg(f.clone() as u64)};
+                    obj.position = obj.position.adjacent(f)
+                }
+            }
+        }
+
         else if let Object::Spawner(spawner) = &mut obj.object {
             if spawner.count == 0 {
                 spawn.push(PositionedObject::new(Object::Monster(Monster::new(10, 1, 1)), obj.position.clone()));
